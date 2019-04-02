6727a4e04edfd01d9ace1f3cb8e5d27d2a547d72
Cherry Yin <yinjuan8019@126.com>
2019-04-02 19:56:47 +0800

    修改主题

--- asset/js/change-bgcolor.js
+++ asset/js/change-bgcolor.js
@@ -5,7 +5,7 @@ var flagX = 0;
     flagY = 2;
 setInterval(function () {
     navBg.style.background = "linear-gradient(" + x +
-        "deg, #3AA17E 10%, #00537E " + y + "%)";
+        "deg, ##FFE6FF 10%, #FF8EFF " + y + "%)";
     if (flagX == 0) {
         x++;
         if (x == 360) {
